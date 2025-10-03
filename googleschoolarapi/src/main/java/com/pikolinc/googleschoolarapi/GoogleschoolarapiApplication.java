f3c7d7ec9f23c021a8c2c7d49771d73fb5056045
Angel Lomeli <alomelibravo@gmail.com>
2025-10-02 19:58:13 -0600

    init: Initial Spring Boot project setup for Challenge 3

---
+++ googleschoolarapi/src/main/java/com/pikolinc/googleschoolarapi/GoogleschoolarapiApplication.java
@@ -0,0 +1,13 @@
+package com.pikolinc.googleschoolarapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoogleschoolarapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoogleschoolarapiApplication.class, args);
+	}
+
+}
---
+++ googleschoolarapi/src/test/java/com/pikolinc/googleschoolarapi/GoogleschoolarapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pikolinc.googleschoolarapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoogleschoolarapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
